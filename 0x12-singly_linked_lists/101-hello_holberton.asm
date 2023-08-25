d33f6e011d65f50b28b7922e0856cda381fc0fbc
danielasele <daniase2113@gmail.com>
2023-08-24 19:45:30 -0700

    Updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0   ; Null-terminated string
-
-section .text
-    global main
-    extern printf
-
+          global   main
+	  extern    printf
 main:
-    sub rsp, 8    ; Align the stack
-    mov rdi, hello
-    call printf
-    add rsp, 8    ; Restore the stack
-
-    mov rax, 60   ; Exit system call
-    xor rdi, rdi  ; Return code 0
-    syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
