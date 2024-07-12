637b49012eaef729d3416139df1c1a74b341cdb7
danielasele <daniase2113@gmail.com>
2024-07-12 11:17:34 -0700

    Add dynamic library and Python test script

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divide - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of a and b, or 0 if b is 0
+ */
+int divide(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0);
+	}
+	return (a / b);
+}
+
+/**
+ * mod - returns the modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of a and b, or 0 if b is 0
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		return (0);
+	}
+	return (a % b);
+}
